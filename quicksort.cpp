cbff5c8e8ccc7e58fe1710c47751a10ffe4c316a
RizkyPratama05 <rizkypp428@gmail.com>
2024-03-28 17:34:46 +0700

    menambahkan program utama

--- quicksort.cpp
+++ quicksort.cpp
@@ -97,3 +97,24 @@ void display()
     cout << "\n\nNumber of comparasions = " << cmp_count << endl;
     cout << "Number of data movements = " << mov_count << endl;
 }
+
+int main()
+{
+    char ch;
+
+    do {
+        input();
+        q_sort(0, n - 1);
+        display();
+        cout << "\n\nDo you want to continue? (y/n) = ";
+        cin >> ch;
+        if (ch == 'n' || ch == 'N')
+            break;
+
+            system("pause");
+            system("CLS");
+
+    } while (true);
+
+    return 0;
+}
